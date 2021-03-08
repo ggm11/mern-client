dc63ce69316923021ac4f54a365c8f6f8268c605
gabriel guerra <gabriel.guerera@vipera.com>
2021-03-08 14:23:22 +0100

    new account

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,5 +1,63 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 function Login() {
-  return <h1>Login</h1>;
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = user;
+
+  const handleOnChange = (e) =>
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+
+  const handleOnSubmit = () => {};
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar Sesion</h1>
+        <form onSubmit={handleOnSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Tu email"
+              value={email}
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="email">Contrasena</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu password"
+              value={password}
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Iniciar Sesion"
+            />
+          </div>
+        </form>
+        <Link to={'/new-account'} className="enlace-cuenta">
+          Obtener cuenta
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- src/components/auth/NewAccount.jsx
+++ src/components/auth/NewAccount.jsx
@@ -1,6 +1,87 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 function NewAccount() {
-    return <h1>NewAccount</h1>;
-  }
-  
-  export default NewAccount;
-  
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirm: '',
+  });
+
+  const { name, email, password, confirm } = user;
+
+  const handleOnChange = (e) =>
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value,
+    });
+
+  const handleOnSubmit = () => {};
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Crear una cuenta</h1>
+        <form onSubmit={handleOnSubmit}>
+          <div className="campo-form">
+            <label htmlFor="email">Nombre</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Tu nombre"
+              value={name}
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Tu email"
+              value={email}
+              onChange={handleOnChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="email">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Tu password"
+              onChange={handleOnChange}
+              value={password}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="email">Confirmar password</label>
+            <input
+              type="password"
+              id="confirm"
+              name="confirm"
+              placeholder="Confirmacion de password"
+              onChange={handleOnChange}
+              value={confirm}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              className="btn btn-primario btn-block"
+              value="Registrar"
+            />
+          </div>
+        </form>
+        <Link to={'/'} className="enlace-cuenta">
+          Volver a iniciar sesion
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default NewAccount;
